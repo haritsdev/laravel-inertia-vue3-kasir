f6bbf13bed3c3200e1341910473e4f1c923953d6
haritsdev <teguhcodedev1@gmail.com>
2022-05-02 18:01:43 +0700

    ADD CRUD CONTROLLERS ROUTE AND FORM FUNCTIONALITES

--- routes/web.php
+++ routes/web.php
@@ -17,22 +17,26 @@ Route::get('/', function () {
     return \Inertia\Inertia::render('Auth/Login');
 })->middleware('guest');
 
-Route::prefix('apps')->group(function(){
+Route::prefix('apps')->group(function () {
 
     //middleware "auth"
-    Route::group(['middleware'=>['auth']],function(){
+    Route::group(['middleware' => ['auth']], function () {
         //route dashboard
-        Route::get('dashboard',App\Http\Controllers\Apps\DashboardController::class)->name('apps.dashboard');
-    
-     //route permissions
-     Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')
-     ->middleware('permission:permissions.index');
+        Route::get('dashboard', App\Http\Controllers\Apps\DashboardController::class)->name('apps.dashboard');
+
+        //route permissions
+        Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')
+            ->middleware('permission:permissions.index');
     });
 
     Route::resource('/roles', \App\Http\Controllers\Apps\RoleController::class, ['as' => 'apps'])
-    ->middleware('permission:roles.index|roles.create|roles.edit|roles.delete');
+        ->middleware('permission:roles.index|roles.create|roles.edit|roles.delete');
 
-      //route resource users
-      Route::resource('/users', \App\Http\Controllers\Apps\UserController::class, ['as' => 'apps'])
-      ->middleware('permission:users.index|users.create|users.edit|users.delete');
+    //route resource users
+    Route::resource('/users', \App\Http\Controllers\Apps\UserController::class, ['as' => 'apps'])
+        ->middleware('permission:users.index|users.create|users.edit|users.delete');
+
+    //route resource categories
+    Route::resource('/categories', \App\Http\Controllers\Apps\CategoryController::class, ['as' => 'apps'])
+        ->middleware('permission:categories.index|categories.create|categories.edit|categories.delete');
 });
